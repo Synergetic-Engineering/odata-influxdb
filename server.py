0a386d01fa7a7f52f651397d7f1c4368027fe296
Steven James <steven@waitforitjames.com>
2017-04-14 15:43:17 -0400

    Complete Refactor
    remove now-unused metadata
    
    remove config.py

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 import pyslet.odata2.metadata as edmx
 from pyslet.odata2.server import ReadOnlyServer
-from influxdb_dal.TimeSeriesCollection import InfluxDatabaseCollection, DatabaseMeasurements, MeasurementPoints, MeasurementCollection
-from influxdb_dal.TimeSeriesCollection import MeasurementDatabase
+from influxdb_dal.influxdbds import InfluxDBEntityContainer
+from generate_metadata import generate_metadata
+from config import INFLUXDB_DSN
 
 SERVICE_PORT = 8080
 SERVICE_ROOT = "http://localhost:%i/" % SERVICE_PORT
@@ -23,24 +24,17 @@ def run_cache_server():
 def load_metadata():
     """Loads the metadata file from the current directory."""
     doc = edmx.Document()
-    with open('InfluxDBSchema.xml', 'rb') as f:
+    with open('generated.xml', 'rb') as f:
         doc.ReadFromStream(f)
     container = doc.root.DataServices['InfluxDBSchema.InfluxDB']
-    container['DatabaseSet'].bind(InfluxDatabaseCollection)
-    container['MeasurementSet'].bind(MeasurementCollection)
-    container['DatabaseSet'].BindNavigation('Measurements', DatabaseMeasurements)
-    container['MeasurementSet'].BindNavigation('Database', MeasurementDatabase)
-    container['MeasurementSet'].BindNavigation('Points', MeasurementPoints)
+    InfluxDBEntityContainer(container=container, dsn=INFLUXDB_DSN)
     return doc
-
-
-def test_model():
-    """Read and write some key value pairs"""
-    doc = load_metadata()
 
 
 def main():
     """Executed when we are launched"""
+    with open('generated.xml', 'wb') as f:
+        f.write(generate_metadata(INFLUXDB_DSN))
     doc = load_metadata()
     server = ReadOnlyServer(serviceRoot=SERVICE_ROOT)
     server.SetModel(doc)
